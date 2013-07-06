0b0ce118528a0707c9708f0a08f74d434077b4a8
Karl Bode <kpbode@gmail.com>
2013-07-06 09:45:27 +0200

    Initial Commit

---
+++ KPBUserDefaultsBackedObject/KPBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KPBAppDelegate.h
+//  KPBUserDefaultsBackedObject
+//
+//  Created by Karl on 7/6/13.
+//  Copyright (c) 2013 Karl Bode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
